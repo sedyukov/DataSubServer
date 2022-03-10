7d2f302b644c0afb5b731619ec0db47f8ea32a2f
sedyukov <62834007+sedyukov@users.noreply.github.com>
2022-03-10 15:25:03 +0300

    Bux fixed

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
+const corsOptions = {
+    origin: '*',
+}
+app.use(cors(corsOptions))
 app.use(express.json({extended: true}))
 app.use('/api/json', require('./routes/json.route'))
 app.listen(PORT, () => {
